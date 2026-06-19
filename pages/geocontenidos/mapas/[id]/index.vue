c5531c91c90bef0b0574a0f455ff547c42dea232
nrrc97 <normanroa97@hotmail.com>
2026-06-19 13:08:36 -0600

    refactor: all map & layer edits can be done on the configuration page of the map.

--- pages/geocontenidos/mapas/[id]/index.vue
+++ pages/geocontenidos/mapas/[id]/index.vue
@@ -17,13 +17,52 @@ const esOwner = computed(() => {
 // Un mapa público solo es editable por su dueño o por un administrador.
 const puedeEditar = computed(() => esAdmin.value || esOwner.value);
 
+const modalEditar = ref(null);
 const modalCompartir = ref(null);
 function abrirCompartir() {
   modalCompartir.value?.abrir();
 }
 
-function abrirAgregarCapas() {
-  mapasStore.abrirModalAgregarCapas();
+// Edición de capas en línea (en vez de navegar a /editar).
+const editandoCapas = ref(false);
+function alternarEdicionCapas() {
+  editandoCapas.value = !editandoCapas.value;
+}
+
+function abrirEditar() {
+  modalEditar.value?.abrir();
+}
+
+async function alternarVisible({ id, visible }) {
+  await mapasStore.actualizarCapa(id, { visible });
+}
+
+async function cambiarOpacidad({ id, opacity }) {
+  await mapasStore.actualizarCapa(id, { opacity });
+}
+
+async function reordenar(orden) {
+  await mapasStore.reordenarCapas(orden);
+}
+
+async function eliminarCapa(id) {
+  await mapasStore.eliminarCapa(id);
+}
+
+async function eliminarMapa() {
+  if (!confirm('¿Eliminar este mapa? Esta acción no se puede deshacer.')) return;
+  const ok = await mapasStore.eliminarMapa(mapaId.value);
+  if (ok) navigateTo('/geocontenidos/mapas');
+}
+
+function cambiarVista(vista) {
+  if (!mapasStore.activeMap) return;
+  mapasStore.activeMap = { ...mapasStore.activeMap, ...vista };
+}
+
+async function guardarVista(vista) {
+  if (!mapasStore.activeMap) return;
+  await mapasStore.actualizarMapa(mapasStore.activeMap.id, vista);
 }
 
 onMounted(async () => {
@@ -59,27 +98,29 @@ onUnmounted(() => {
             class="boton-secundario"
             target="_blank"
           >
-            <i class="fa-solid fa-eye" aria-hidden="true"></i> Visualizar
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>&nbsp;Visualizar
           </NuxtLink>
           <button class="boton-secundario" type="button" @click="abrirCompartir">
-            <i class="fa-solid fa-share-nodes" aria-hidden="true"></i> Compartir
+            <i class="fa-solid fa-share-nodes" aria-hidden="true"></i>&nbsp;Compartir
+          </button>
+          <button v-if="puedeEditar" class="boton-primario" type="button" @click="abrirEditar">
+            <span class="pictograma-mas" aria-hidden="true" /> Propiedades del Mapa
           </button>
           <button
             v-if="puedeEditar"
-            class="boton-primario"
-            type="button"
-            @click="abrirAgregarCapas"
-          >
-            <span class="pictograma-mas" aria-hidden="true" /> Agregar capas
-          </button>
-          <NuxtLink
-            v-if="puedeEditar"
-            :to="`/geocontenidos/mapas/${mapaId}/editar`"
             class="boton-secundario"
+            :class="{ 'boton-primario': editandoCapas }"
+            type="button"
+            :aria-pressed="editandoCapas"
+            @click="alternarEdicionCapas"
           >
-            <span class="pictograma-editar" aria-hidden="true" /> Editar
-          </NuxtLink>
-          <NuxtLink to="/geocontenidos/mapas" class="boton-secundario">Volver</NuxtLink>
+            <span class="pictograma-editar" aria-hidden="true" />
+            &nbsp;{{ editandoCapas ? 'Cerrar Edición' : 'Editar Capas' }}
+          </button>
+          <NuxtLink to="/geocontenidos/mapas" class="boton-secundario">Lista de Mapas</NuxtLink>
+          <button class="boton-secundario boton-eliminar" type="button" @click="eliminarMapa">
+            <span class="pictograma-tache" aria-hidden="true" /> Eliminar mapa
+          </button>
         </div>
       </header>
 
@@ -89,22 +130,37 @@ onUnmounted(() => {
             v-if="mapasStore.activeMap.map_type === 'regular'"
             :mapa="mapasStore.activeMap"
             :capas="mapasStore.activeLayers"
+            @vista="cambiarVista"
           />
           <MapasVisorSwipe
             v-else-if="mapasStore.activeMap.map_type === 'swipe'"
             :mapa="mapasStore.activeMap"
             :capas="mapasStore.activeLayers"
+            @vista="cambiarVista"
           />
           <MapasVisorDual
             v-else-if="mapasStore.activeMap.map_type === 'dual'"
             :mapa="mapasStore.activeMap"
             :capas="mapasStore.activeLayers"
+            @vista="cambiarVista"
           />
         </div>
 
-        <MapasPanelCapas :capas="mapasStore.activeLayers" :editable="false" />
+        <MapasPanelCapas
+          :capas="mapasStore.activeLayers"
+          :editable="editandoCapas"
+          :mapa="mapasStore.activeMap"
+          @toggle="alternarVisible"
+          @opacidad="cambiarOpacidad"
+          @reordenar="reordenar"
+          @eliminar="eliminarCapa"
+          @agregar="abrirAgregarCapas"
+          @vista="cambiarVista"
+          @guardar-vista="guardarVista"
+        />
       </div>
 
+      <MapasModalEditarMapa ref="modalEditar" :mapa="mapasStore.activeMap" />
       <MapasModalCompartir ref="modalCompartir" :mapa="mapasStore.activeMap" />
     </template>
   </div>
@@ -134,7 +190,16 @@ a {
   border: 3px solid #e5c743;
 }
 
+.boton-eliminar {
+  background-color: rgb(136, 0, 0);
+}
+
+.boton-eliminar:hover {
+  background-color: rgb(191, 46, 46);
+}
+
 .flex {
-  gap: 0px;
+  gap: 8px;
+  flex-wrap: wrap;
 }
 </style>
